0ba59a58549b9a258dd376c2d112e7802f847984
Geobo <bozhys@gmail.com>
2022-01-22 17:56:47 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let cycleCounter = 0;
+    let prevLength;
+    let bracketsPairs = bracketsConfig.map (item => item.join (''));
+
+    while (str.length !== 0) {
+        prevLength = str.length;
+        bracketsPairs.forEach (item => {
+            str = str.split (item)
+                     .join ('');
+        })
+
+        if (str.length == prevLength) return false;
+        if (cycleCounter > 100) return 'error: it\'s cycle';
+        cycleCounter++;
+    }
+    return true;
 }
